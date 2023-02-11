a4f90d1b5af6a13e8b6e43937d1d3c1acaced481
mavel <p.m.berdnikov@gmail.com>
2023-02-11 22:56:44 +0300

    clean up

--- train_agent.py
+++ train_agent.py
@@ -1,59 +1,30 @@
 import json
 import logging
-from pathlib import Path
 
 import google
 from environs import Env
 from google.cloud import dialogflow
 
-
-def list_intents(project_id):
-    intents_client = dialogflow.IntentsClient()
-
-    parent = dialogflow.AgentsClient.agent_path(project_id)
-
-    intents = intents_client.list_intents(request={"parent": parent})
-
-    for intent in intents:
-        print("=" * 20)
-        print("Intent name: {}".format(intent.name))
-        print("Intent display_name: {}".format(intent.display_name))
-        print("Action: {}\n".format(intent.action))
-        print("Root followup intent: {}".format(intent.root_followup_intent_name))
-        print("Parent followup intent: {}\n".format(intent.parent_followup_intent_name))
-
-        print("Input contexts:")
-        for input_context_name in intent.input_context_names:
-            print("\tName: {}".format(input_context_name))
-
-        print("Output contexts:")
-        for output_context in intent.output_contexts:
-            print("\tName: {}".format(output_context.name))
+logger = logging.getLogger(__name__)
 
 
 def create_intent(project_id, display_name, training_phrases_parts, answer_text):
     intents_client = dialogflow.IntentsClient()
-
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
-        # Here we create a new training phrase for each provided part.
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
-
     text = dialogflow.Intent.Message.Text(text=[answer_text])
     message = dialogflow.Intent.Message(text=text)
-
     intent = dialogflow.Intent(
         display_name=display_name, training_phrases=training_phrases, messages=[message]
     )
-
     response = intents_client.create_intent(
-        request={"parent": parent, "intent": intent}
+        request={'parent': parent, 'intent': intent}
     )
-
-    print("Intent created: {}".format(response))
+    logger.info(f'Intent created: {response}')
 
 
 def main():
@@ -67,7 +38,7 @@ def main():
         gc_credentials = json.load(file)
         gc_project_id = gc_credentials['project_id']
 
-    with open(Path('data/intents.json'), 'r') as file:
+    with open('data/intents.json', 'r') as file:
         intents = json.load(file)
 
     for intent_name, intent_settings in intents.items():
